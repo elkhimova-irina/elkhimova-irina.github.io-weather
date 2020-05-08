84a84d9bf8e1e02b0ebaf882992e29b86244ffe4
elkhimova-irina <elkhimova-irina@mail.ru>
2020-05-08 12:26:37 +0300

    Initial commit

---
+++ lab4/App.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCjluBzHJGoIfw6Ip8n-rMV4n2HJPrD5cg",
+  authDomain: "temporary-pass.firebaseapp.com",
+  databaseURL: "https://temporary-pass.firebaseio.com",
+  projectId: "temporary-pass",
+  storageBucket: "temporary-pass.appspot.com",
+  messagingSenderId: "571693663925",
+  appId: "1:571693663925:web:5b17a3e99ede96d4b320fc",
+  measurementId: "G-MGL6M4DCEE"
+};
+
+
+  firebase.initializeApp(firebaseConfig);
